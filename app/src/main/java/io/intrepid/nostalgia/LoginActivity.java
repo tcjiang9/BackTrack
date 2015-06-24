9bc56d6d42fc7b7253ed61acfd69f55f0c69c5a2
priyakarambelkar <piyu004@gmail.com>
2015-06-24 16:25:24 -0400

    Adding skip login functionality (WIP)

--- app/src/main/java/io/intrepid/nostalgia/LoginActivity.java
+++ app/src/main/java/io/intrepid/nostalgia/LoginActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
@@ -22,21 +25,18 @@ import butterknife.InjectView;
 import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
-    public static String accessToken = "accessToken";
-    public static boolean isFacebook = false;
+    public  static boolean isFacebook = false;
+    public  static  String accessToken = "access";
     CallbackManager callbackManager;
     FacebookCallback<LoginResult> facebookCallback;
+    Button skipFacebook;
     @InjectView(R.id.skip_facebook)
     Button getSkipFacebook;
-    private AccessTokenTracker accessTokenTracker;
-    // name of token can be accessed from other classes for SharedPrefs
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(this);
         setContentView(R.layout.activity_login);
-       // runActivityOnce();
         ButterKnife.inject(this);
         setupFacebook();
     }
@@ -96,6 +96,6 @@ public class LoginActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
-
     }
+
 }
