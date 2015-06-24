308f14e01b2a5bf1bcc7498118c38853572dc409
priyakarambelkar <piyu004@gmail.com>
2015-06-24 15:34:23 -0400

    facebook sdk integrated successfully

--- app/src/main/java/io/intrepid/nostalgia/LoginActivity.java
+++ app/src/main/java/io/intrepid/nostalgia/LoginActivity.java
@@ -1,4 +1,105 @@
 package io.intrepid.nostalgia;
 
-public class LoginActivity {
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import butterknife.InjectView;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    CallbackManager callbackManager;
+    @InjectView(R.id.skip_facebook)
+    Button skipFacebook;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this);
+        setContentView(R.layout.activity_login);
+        setupFacebook();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    FacebookCallback<LoginResult> facebookCallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+            if (profile != null){
+                Toast.makeText(getApplicationContext(), "Logged in as : "+profile.getFirstName(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+
+        }
+    };
+    private void setupFacebook() {
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton facebookLogin = (LoginButton)findViewById(R.id.login_button);
+        facebookLogin.setOnClickListener(this);
+        facebookLogin.setReadPermissions("public_profile","read_stream", "user_posts");
+        facebookLogin.registerCallback(callbackManager,facebookCallback);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+    }
 }
