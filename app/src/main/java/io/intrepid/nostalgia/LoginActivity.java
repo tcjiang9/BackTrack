2bb4c563a470b8fa122b8b4bc5296da7f40b2b4d
priyakarambelkar <piyu004@gmail.com>
2015-06-24 15:50:37 -0400

    removed auto generated methods from code

--- app/src/main/java/io/intrepid/nostalgia/LoginActivity.java
+++ app/src/main/java/io/intrepid/nostalgia/LoginActivity.java
@@ -1,21 +1,11 @@
 package io.intrepid.nostalgia;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -24,9 +14,6 @@ import com.facebook.FacebookSdk;
 import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 import butterknife.InjectView;
 
@@ -42,35 +29,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         setupFacebook();
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     FacebookCallback<LoginResult> facebookCallback = new FacebookCallback<LoginResult>() {
         @Override
         public void onSuccess(LoginResult loginResult) {
             AccessToken accessToken = loginResult.getAccessToken();
             Profile profile = Profile.getCurrentProfile();
-            if (profile != null){
-                Toast.makeText(getApplicationContext(), "Logged in as : "+profile.getFirstName(), Toast.LENGTH_LONG).show();
+            if (profile != null) {
+                Toast.makeText(getApplicationContext(), "Logged in as : " + profile.getFirstName(), Toast.LENGTH_LONG).show();
             }
         }
 
@@ -84,18 +50,20 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         }
     };
+
     private void setupFacebook() {
         callbackManager = CallbackManager.Factory.create();
-        LoginButton facebookLogin = (LoginButton)findViewById(R.id.login_button);
+        LoginButton facebookLogin = (LoginButton) findViewById(R.id.login_button);
         facebookLogin.setOnClickListener(this);
-        facebookLogin.setReadPermissions("public_profile","read_stream", "user_posts");
-        facebookLogin.registerCallback(callbackManager,facebookCallback);
+        facebookLogin.setReadPermissions("public_profile", "read_stream", "user_posts");
+        facebookLogin.registerCallback(callbackManager, facebookCallback);
     }
 
     @Override
     public void onClick(View v) {
 
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
