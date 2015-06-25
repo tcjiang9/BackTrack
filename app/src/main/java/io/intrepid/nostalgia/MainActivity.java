c162247860ef107e5d191d619cbd6357f44d89d6
Hayley Hansson <hayleyhansson@Hayleys-MacBook-Pro.local>
2015-06-25 14:31:00 -0400

    added facebook accessed check to hide or show facebook activity

--- app/src/main/java/io/intrepid/nostalgia/MainActivity.java
+++ app/src/main/java/io/intrepid/nostalgia/MainActivity.java
@@ -1,10 +1,14 @@
 package io.intrepid.nostalgia;
 
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -15,11 +19,17 @@ import butterknife.InjectView;
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    @InjectView(R.id.facebook_view)
+    RelativeLayout facebookView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (preferences.getString("accessToken", null) == null){
+            facebookView.setVisibility(View.GONE);
+        }
     }
 
 
