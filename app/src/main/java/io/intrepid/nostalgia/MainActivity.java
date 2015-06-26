528af97dde39c143973adce621015f3bf68ba729
Tiancheng Jiang <tjiang@Tianchengs-MacBook-Pro.local>
2015-06-26 11:23:23 -0400

    Implementing Phil's comments

--- app/src/main/java/io/intrepid/nostalgia/MainActivity.java
+++ app/src/main/java/io/intrepid/nostalgia/MainActivity.java
@@ -1,29 +1,15 @@
 package io.intrepid.nostalgia;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.RelativeLayout;
 
-import java.util.Calendar;
-
-import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
 
-    /**
-     * Year that appears upon first logging in. The previous year from current
-     */
-    private static final int DEFAULT_YEAR =  Calendar.getInstance().get(Calendar.YEAR) - 1852;
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(new YearCollectionPagerAdapter(getSupportFragmentManager()));
-        viewPager.setCurrentItem(DEFAULT_YEAR);
+        viewPager.setCurrentItem(Constants.DEFAULT_YEAR);
     }
 
 
@@ -56,30 +42,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public static class YearCollectionPagerAdapter extends FragmentStatePagerAdapter{
-        public YearCollectionPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int i) {
-            Fragment fragment = new NostalgiaFragment();
-            Bundle args = new Bundle();
-            args.putInt(NostalgiaFragment.YEAR, i + 1852);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public int getCount() {
-            return DEFAULT_YEAR; //The number of years to display since 1851
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return String.valueOf(position + 1852);
-        }
     }
 }
--- app/src/main/java/io/intrepid/nostalgia/YearFragment.java
+++ app/src/main/java/io/intrepid/nostalgia/YearFragment.java
@@ -3,24 +3,21 @@ package io.intrepid.nostalgia;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class NostalgiaFragment extends Fragment{
+public class YearFragment extends Fragment {
     public static final String YEAR = "Display Year";
 
     @InjectView(R.id.facebook_view)
     RelativeLayout facebookView;
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         int currentYear = getArguments().getInt(YEAR);
@@ -29,7 +26,7 @@ public class NostalgiaFragment extends Fragment{
         ButterKnife.inject(this, rootView);
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        if (preferences.getString("accessToken", null) == null){
+        if (preferences.getString(Constants.SHARED_PREFS_ACCESS_TOKEN, null) == null) {
             facebookView.setVisibility(View.GONE);
         }
         return rootView;
