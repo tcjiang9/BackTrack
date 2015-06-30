f24503d44fbcc1fc1bab1f9af8462cdf67a755d8
priyakarambelkar <piyu004@gmail.com>
2015-06-30 09:19:00 -0400

    working on clickable iv to open post details

---
+++ app/src/main/java/io/intrepid/nostalgia/FacebookDetailsActivity.java
@@ -0,0 +1,18 @@
+package io.intrepid.nostalgia;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class FacebookDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_details);
+    }
+
+}
