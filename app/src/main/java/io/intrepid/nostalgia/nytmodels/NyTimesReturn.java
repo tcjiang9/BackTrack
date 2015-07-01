b58922e35b4a070c11edbc511aa81d8c2aee0db1
Hayley Hansson <hayleyhansson@Hayleys-MacBook-Pro.local>
2015-07-01 10:43:13 -0400

    fixing stylistic errors in pojos

--- app/src/main/java/io/intrepid/nostalgia/nytmodels/NyTimesReturn.java
+++ app/src/main/java/io/intrepid/nostalgia/nytmodels/NyTimesReturn.java
@@ -4,35 +4,10 @@ import com.google.gson.annotations.Expose;
 
 public class NyTimesReturn {
 
-    @Expose
+
     private Response response;
-    @Expose
-    private String status;
-    @Expose
-    private String copyright;
 
     public Response getResponse() {
         return response;
     }
-
-    public void setResponse(Response response) {
-        this.response = response;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getCopyright() {
-        return copyright;
-    }
-
-    public void setCopyright(String copyright) {
-        this.copyright = copyright;
-    }
-
 }
--- app/src/main/java/io/intrepid/nostalgia/nytmodels/Response.java
+++ app/src/main/java/io/intrepid/nostalgia/nytmodels/Response.java
@@ -6,24 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Response {
-    @Expose
-    private Meta meta;
-    @Expose
+    
     private List<Doc> docs = new ArrayList<Doc>();
-
-    public Meta getMeta() {
-        return meta;
-    }
-
-    public void setMeta(Meta meta) {
-        this.meta = meta;
-    }
 
     public List<Doc> getDocs() {
         return docs;
-    }
-
-    public void setDocs(List<Doc> docs) {
-        this.docs = docs;
     }
 }
