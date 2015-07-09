59fd1a4049c8f2312440d8b7219ac07c86887a67
Tiancheng Jiang <tjiang@Tianchengs-MacBook-Pro.local>
2015-07-09 11:11:02 -0400

    Merge changes from thread fix branch

--- app/src/main/java/io/intrepid/nostalgia/SinglePlayer.java
+++ app/src/main/java/io/intrepid/nostalgia/SinglePlayer.java
@@ -2,7 +2,6 @@ package io.intrepid.nostalgia;
 
 public class SinglePlayer {
     private static android.media.MediaPlayer theMediaPlayer;
-    //private static boolean isPreparing;
 
     public static android.media.MediaPlayer getInstance() {
         if (theMediaPlayer == null) {
