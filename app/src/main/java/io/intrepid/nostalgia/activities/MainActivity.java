188972e5a26abcc5dd2e7d6769b39eadfc32adac
priya <piyu004@gmail.com>
2015-07-14 17:07:23 -0400

    working code for navigation bg

--- app/src/main/java/io/intrepid/nostalgia/activities/MainActivity.java
+++ app/src/main/java/io/intrepid/nostalgia/activities/MainActivity.java
@@ -1,7 +1,7 @@
 package io.intrepid.nostalgia.activities;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
