12f90213fe6bfb3c70a7288e59bc9c1bf899cc4e
priya <piyu004@gmail.com>
2015-07-16 13:22:26 -0400

    temp

--- app/src/main/java/io/intrepid/nostalgia/fragments/NewsFragment.java
+++ app/src/main/java/io/intrepid/nostalgia/fragments/NewsFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.ButterKnife;
@@ -32,6 +33,9 @@ public class NewsFragment extends Fragment {
 
     public String url;
 
+    @InjectView(R.id.ufo_image_transition)
+    ImageView ufoTransition;
+
     @InjectView(R.id.news_headline)
     TextView newsHeadline;
 
@@ -52,6 +56,8 @@ public class NewsFragment extends Fragment {
         i.putExtra(Intent.EXTRA_TEXT, url);
         startActivity(Intent.createChooser(i, SHARE_URL));
     }
+
+    String CURRENT_YEAR;
 
     @OnClick(R.id.read_more_nyt)
     void onReadMore() {
@@ -77,10 +83,10 @@ public class NewsFragment extends Fragment {
     }
 
     public void sendNytGetRequest() {
-        final String CURRENT_YEAR = Integer.toString(getArguments().getInt(YEAR));
+        CURRENT_YEAR = Integer.toString(getArguments().getInt(YEAR));
         String date = DateFormatter.makeNytDate(CURRENT_YEAR);
         Log.d(TAG, "@@@@@@@@@@@@@@@@@@@@@@@" + CURRENT_YEAR);
-
+        changeUfoImage();
         NytServiceAdapter.getNytServiceInstance()
                 .getNytArticle(date, date, new Callback<NyTimesReturn>() {
                     @Override
@@ -98,14 +104,29 @@ public class NewsFragment extends Fragment {
                         newsByline.setText(by);
                         newsBody.setText(snippet);
                         ribbonDate.setText(DateFormatter.makeRibbonDateText());
-                        Log.d(TAG,"!!!!!!!!!!!!!!!!!!!!!!!!!!!!" + pubDate + webUrl + headline + snippet);
+                        Log.d(TAG, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!" + pubDate + webUrl + headline + snippet);
                     }
 
                     @Override
                     public void failure(RetrofitError error) {
-                        Log.d(TAG,"!!!!!!!!!!!!!!!!!!!!!!!!!!!" + error.toString());
+                        Log.d(TAG, "!!!!!!!!!!!!!!!!!!!!!!!!!!!" + error.toString());
                     }
                 });
 
+    }
+
+    private void changeUfoImage() {
+        int constantYr = 1980;
+        int currentYer = Integer.parseInt(CURRENT_YEAR);
+        int difference = currentYer - constantYr;
+        if (difference >= 30) {
+            ufoTransition.setImageResource(R.drawable.ufo);
+        } else if (difference >= 20) {
+            ufoTransition.setImageResource(R.drawable.rocket_2000);
+        } else if (difference >= 10) {
+            ufoTransition.setImageResource(R.drawable.plane_90s);
+        } else if (difference >= 0 || difference < 0) {
+            ufoTransition.setImageResource(R.drawable.hot_balloon_80s);
+        }
     }
 }
