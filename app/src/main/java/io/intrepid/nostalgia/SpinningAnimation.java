d330e38a073cc71d3641525156aa4b85b5af737c
Tiancheng Jiang <tjiang@Tianchengs-MacBook-Pro.local>
2015-07-17 10:51:51 -0400

    Adding rotation animation class

---
+++ app/src/main/java/io/intrepid/nostalgia/SpinningAnimation.java
@@ -0,0 +1,12 @@
+package io.intrepid.nostalgia;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.animation.RotateAnimation;
+
+public class SpinningAnimation extends RotateAnimation {
+
+    public SpinningAnimation(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
