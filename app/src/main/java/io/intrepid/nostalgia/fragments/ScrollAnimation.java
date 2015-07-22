9629dc8c82be2e257ba6aa8b21e5ce07b732e5d9
priya <piyu004@gmail.com>
2015-07-22 09:36:40 -0400

    cleanup for PR

--- app/src/main/java/io/intrepid/nostalgia/fragments/ScrollAnimation.java
+++ app/src/main/java/io/intrepid/nostalgia/fragments/ScrollAnimation.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
